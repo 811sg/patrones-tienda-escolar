be6267f4d9b81df15be7fabf74900a819fe55792
811sg <sg728203@gmail.com>
2026-09-04 21:08:34 -0500

    modificaciones actividad

--- src/patterns/state/PendienteState.ts
+++ src/patterns/state/PendienteState.ts
@@ -1,19 +1,17 @@
 import type { OrderState } from "./OrderState";
 import type { PedidoContext } from "./PedidoContext";
 import { CanceladoState } from "./CanceladoState";
-import { EnRevisionState } from "./EnRevisionState";
+import { PagadoState } from "./PagadoState";
 
 /** PATRÓN: State — Rol: "ConcreteState" */
 export class PendienteState implements OrderState {
-
   readonly name = "Pendiente";
 
   avanzar(context: PedidoContext): void {
-    context.setState(new EnRevisionState());
+    context.setState(new PagadoState());
   }
 
   cancelar(context: PedidoContext): void {
     context.setState(new CanceladoState());
   }
-
 }
